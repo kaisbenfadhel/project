1cac1f0893d0cb7e3e8428a7597f298f84a5530b
kaisbenfadhel <ben.fadhel.kais@gmail.com>
2019-08-29 11:31:21 +0200

    test 2

---
+++ project/src/main/scala/MyFirstScalaSpark.scala
@@ -0,0 +1,21 @@
+package com.example
+import org.apache.spark.sql.SparkSession
+object MyFirstScalaSpark {
+  def main(args: Array[String]) {
+    val spark = SparkSession.builder
+      .appName("MyFirstScalaSpark")
+      .getOrCreate()
+    val filePath = args(0)
+    val data = spark.read
+      .option("header", true)
+      .option("inferSchema", true)
+      .option("timestampFormat", "dd/MM/yyyy")
+      .csv(filePath)
+    println(data.count)
+    data.printSchema
+    data.show
+    val orderedData = data.orderBy(("Date of Payment"))
+    orderedData.show(5)
+    spark.stop()
+  }
+}
